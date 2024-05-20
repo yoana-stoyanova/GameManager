77861da8a69e4aa6577c5f40100656f49ec6a5ff
yoana stoyanova <82516108+yoana-stoyanova@users.noreply.github.com>
2024-05-20 19:12:15 +0300

    Add files via upload

---
+++ GameManager3/GameManager/src/GameManager.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+public class GameManager extends Methods{
+    public static void main(String[] args) {
+
+        ArrayList<User> userList = new ArrayList<>();
+        ArrayList<Game> gameList = new ArrayList<>();
+
+        createUserList(userList);
+
+        showInfoByUsername(userList, "@eva-hristova");
+
+
+    }
+
+
+}
